e66a5bf3da82bf99e1dc1fbc9bdfe5ac27aa9f75
Luiza Santiago <lu.sntgo@gmail.com>
2022-03-07 13:31:47 -0300

    feat: get and add rentalss

--- src/Controllers/rentalsController.js
+++ src/Controllers/rentalsController.js
@@ -1,27 +1,79 @@
 import db from "../db.js";
+import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
+  const { customerId, gameId } = req.query;
   try {
-    const { rows: rentals } = db.query("SELECT * FROM rentals ");
-    res.send(rentals);
+    const { rows: rentals } = await db.query(`
+    SELECT 
+      rentals.*,
+      customers.name AS "customerName",
+      customer.id AS "customerId",
+      games.id AS "gameId",
+      games.name AS "gameName",
+      games."categoryId" categories.name AS "categoryName" 
+    FROM rentals
+      JOIN customers ON rentals."customerId" = customers.id
+      JOIN games ON rentals."gamesId" = games.id
+      JOIN categories ON games."categoryId" = categories.id
+      ${customerId ? `WHERE customers.id = ${parseInt(customerId)}` : ""}
+      ${gameId ? `WHERE games.id = ${parseInt(gameId)}` : ""}
+    `);
+
+    const listRentals = rentals.map((r) => {
+      const entry = {
+        ...r,
+        rentDate: dayjs(r.rentDate).format("YYYY-MM-DD"),
+        customer: {
+          id: r.customerId,
+          name: r.customerName,
+        },
+        game: {
+          id: r.gameId,
+          name: r.gameName,
+          categoryId: r.categoryId,
+          categoryName: r.categoryName,
+        },
+      };
+      delete entry.customerId;
+      delete entry.customerName;
+
+      delete entry.gameId;
+      delete entry.gameName;
+
+      delete entry.categoryId;
+      delete entry.categoryName;
+
+      return entry;
+    });
+    res.send(listRentals);
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
   }
 }
 
+export async function addRentals(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
+  const rentDate = dayjs().format('YYYY-MM-DD');
 
-export async function addRentals(req,res){
-  const {customerId, gameId, daysRented } = req.body;
-try{
-  db.query(`
+  try {
+    const{ rows: pricePerDay } = await db.query(`
+    SELECT 
+      games."pricePerDay" 
+    FROM games 
+      WHERE id=$1
+    `,[gameId]);
+
+    await db.query(`
     INSERT INTO 
-        rentals ("gameId", "customerId", "daysRented")
-    VALUES ($1, $2, $3)
-    `[customerId, gameId, daysRented])
-    res.sendStatus(201)
+      rentals("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")  
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+    `,[customerId, gameId, rentDate, daysRented, null, pricePerDay.pricePerDay * daysRented, null]);
 
-} catch(error) {
-    console.log(error)
-    res.status(500).send(error)
-}}
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
+
